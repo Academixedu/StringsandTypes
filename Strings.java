d53958fbbeafebe2c8d49ae2e3574f36c3767076
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-28 19:03:44 +0530

    Swarupa

--- Strings.java
+++ Strings.java
@@ -1,3 +1,12 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+ public static String reverse(){
+    String str="SWARUPA JANGAM";
+    return new StringBuilder(str).reverse().toString();
+ }
+ public static void main(String[] args) {
+    String s= reverse();
+    System.out.println(s);
+
+ }
 }
