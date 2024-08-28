f5ae739417288cc5f1ee86d5710e183692710163
psvsai <pasupuletisvsm245@gmail.com>
2024-08-28 20:04:36 +0530

    PSVSAI

--- StringBuff.java
+++ StringBuff.java
@@ -3,4 +3,14 @@ public class StringBuff{
   // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
   // Delete World using delete();
+ public static void main(String[] args) {
+  StringBuilder sb = new StringBuilder("Hello World");
+  String sub = sb.substring(0, 5);
+  System.out.println(sub);
+  sb.insert(5, ",");
+  System.out.println(sb.toString());
+  sb.delete(5, 12);
+  System.out.println(sb.toString());
+
+ }
 }
--- Strings.java
+++ Strings.java
@@ -1,3 +1,18 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+ public static String reverseString(String name){
+    StringBuilder sb = new StringBuilder(name);
+    sb.reverse();
+    return sb.toString(); 
+}
+
+public static void main(String[] args) {
+
+    String naam = "PSVAI";
+    
+    String rev = reverseString(naam);
+    System.out.println(rev);
+
+  
+}
 }
