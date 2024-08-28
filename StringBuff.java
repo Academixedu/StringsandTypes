7dbba3962007e19e38309fee5e9ea5d84d2ea093
sujatha-94 <sujathakarri701@gmail.com>
2024-08-28 18:40:33 +0530

    programs

--- StringBuff.java
+++ StringBuff.java
@@ -3,4 +3,14 @@ public class StringBuff{
   // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
   // Delete World using delete();
+  public static void main(String[] args) {
+  
+  StringBuilder sb = new StringBuilder("Hello World");
+   
+   System.out.println(sb.substring(0, 5)); 
+   sb.insert(5, ",");
+   System.out.println("After inserting comma: " + sb); 
+   sb.delete(7, 12);
+   System.out.println("After deletion world: " + sb);
+}
 }
--- Strings.java
+++ Strings.java
@@ -1,3 +1,12 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+public static String reverse(String str) {
+    StringBuilder sb = new StringBuilder(str);
+    return sb.reverse().toString();
+}
+public static void main(String[] args) {
+    String s = "Hello World";
+    System.out.println("Original String: " + s);
+    System.out.println("Reversed String: " + reverse(s));
+}
 }
