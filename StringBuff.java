4529c516b4c8ed1755afcd8cf941345e5560d219
preetham0223 <sagilinoyal@gmail.com>
2024-08-28 19:38:59 +0530

    commit

--- StringBuff.java
+++ StringBuff.java
@@ -1,4 +1,12 @@
 public class StringBuff{
+  public static void main(String[] args) {
+    String s="Hello World";
+    StringBuilder sb=new StringBuilder(s);
+    
+    System.out.println(sb.substring(0,5));
+    System.out.println(sb.insert(5,","));
+    System.out.println(sb.delete(6, 12));
+  }
 // Create a String Builder add String Vaalue is "Hello World" 
   // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
--- Strings.java
+++ Strings.java
@@ -1,3 +1,13 @@
 public class Strings{
+    public String reverse(String s){
+        StringBuilder sr=new StringBuilder(s);
+        sr.reverse();
+        return sr.toString();
+    }
+    public static void main(String[] args) {
+        Strings a=new Strings();
+        String reversed=a.reverse("ayoN");
+        System.out.println(reversed);
+    }
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
 }
