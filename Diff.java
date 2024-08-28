fbe2023b76d78ef617bca1439b5d7128e353eab6
siva-245711 <sivanagendra809@gmail.com>
2024-08-28 20:21:47 +0530

    strings

--- Diff.java
+++ Diff.java
@@ -1,3 +1,14 @@
 public class Diff{
 // Show the Difference Between String and StringBuilder
+public static void main(String[] args) {
+    String s = "Hello";
+    StringBuilder strbui = new StringBuilder("Hello");
+    System.out.println(s);
+    System.out.println(strbui);
+
+    s = s + " World";
+    strbui.append(" World");
+    System.out.println(s);
+    System.out.println(strbui);
+}
 }
--- StringBuff.java
+++ StringBuff.java
@@ -3,4 +3,13 @@ public class StringBuff{
   // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
   // Delete World using delete();
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("Hello World");
+    System.out.println(sb.substring(0, 5));
+    sb.insert(5, ",");
+    System.out.println(sb);
+    sb.delete(6, 12);
+    System.out.println(sb);
+  }
 }
