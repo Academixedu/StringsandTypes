7b3c917bc9a8dc70a747a7c024789319a34df53a
jahnavi0545 <kandiboyinajahnavi@gmail.com>
2024-08-28 19:21:10 +0530

    stringsandtypes

--- Strings.java
+++ Strings.java
@@ -1,3 +1,20 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+public static String reverse(String input) {
+    StringBuilder sb = new StringBuilder(input);
+    return sb.reverse().toString();
+}
+
+public static void main(String[] args) {
+
+    String original = "Hello World";
+
+
+    String reversed = reverse(original);
+
+
+    System.out.println("Original String: " + original);
+    System.out.println("Reversed String: " + reversed);
+}
+
 }
