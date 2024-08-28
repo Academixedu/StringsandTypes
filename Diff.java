cec136bdd981557c4d75eeaf99465e7da4bd1a4e
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-08-28 21:01:03 +0530

    MANI

--- Diff.java
+++ Diff.java
@@ -1,3 +1,11 @@
 public class Diff{
 // Show the Difference Between String and StringBuilder
+public static void main(String args[]){
+    String s="Hello world";
+    StringBuilder a=new StringBuilder(s);
+    System.out.println("String is immutable it throws an error when we try to insert");
+   // System.out.println(s.insert(5,','));//
+   System.out.println("String Builder is mutable");
+   System.out.println(a.insert(5,','));
+}
 }
--- StringBuff.java
+++ StringBuff.java
@@ -3,4 +3,13 @@ public class StringBuff{
   // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
   // Delete World using delete();
+  public static void main(String args[]){
+    String value="Hello World";
+    StringBuilder s=new StringBuilder(value);
+    System.out.println(s.substring(0,5));
+    System.out.println(s.insert(5, ','));
+    System.out.println(s.delete(6, 12));
+
+
+  }
 }
