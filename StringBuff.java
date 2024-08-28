67e5717f9b265db636b4a5aab0f46c43f63ca303
gsbhavani <suryagogadi02@gmail.com>
2024-08-28 19:35:56 +0530

    done

--- StringBuff.java
+++ StringBuff.java
@@ -3,4 +3,18 @@ public class StringBuff{
   // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
   // Delete World using delete();
+
+  public static void main(String[] args) {
+
+    StringBuilder sb = new StringBuilder("Hello World");
+    System.out.println(sb);
+    System.out.println(sb.substring(0, 5));
+    sb.insert(5, ",");
+    System.out.println(sb);
+    sb.delete(5, 9);
+    System.out.println(sb);
+
+
+
+  }
 }
--- Strings.java
+++ Strings.java
@@ -1,3 +1,15 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+
+public static void main(String[] args) {
+    String str = "Hello World";
+    System.out.println(str);
+    System.out.println(reverse(str));
+}
+public static String reverse(String str) {
+    for (int i = str.length() - 1; i >= 0; i--) {
+        System.out.print(str.charAt(i));
+    }
+    return "";
+}
 }
