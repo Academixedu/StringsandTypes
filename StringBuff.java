5e5b42232835d1117ff83b01c500d22add5574f2
Swapna-321 <gowdaperuswapna@gmail.com>
2024-08-28 10:36:57 -0400

    string

--- StringBuff.java
+++ StringBuff.java
@@ -1,6 +1,19 @@
 public class StringBuff{
-// Create a String Builder add String Vaalue is "Hello World" 
-  // Print Hello using SubString()
+ // Print Hello using SubString()
   // Add ,  between Hello and World uisng Insert()
   // Delete World using delete();
+
+  public static void main(String[] args) {
+
+    StringBuilder sb = new StringBuilder("Hello World");
+    System.out.println(sb);
+    System.out.println(sb.substring(0, 5));
+    sb.insert(5, ",");
+    System.out.println(sb);
+    sb.delete(5, 9);
+    System.out.println(sb);
+
+
+
+  }
 }
