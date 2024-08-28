12a685bebe289ad097698cd631f7b1a63eb32335
srinadh0405 <srinadh.nallabothula@email.com>
2024-08-28 19:50:29 +0530

    String&types

--- Strings.java
+++ Strings.java
@@ -1,3 +1,19 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+
+     public static void main(String[] args) {
+
+        String s = "programming";
+        System.out.println(reverse(s));
+     }
+
+     public static String reverse(String s){
+
+        for(int i=s.length()-1;i>=0;i--){
+
+            System.out.println(s.charAt(i));
+
+        }
+        return" ";
+     }
 }
