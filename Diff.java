893044de883a3bbb353c597a91d061c72d22afa9
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-28 18:46:09 +0530

    Update Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,16 @@
-public class Diff{
-// Show the Difference Between String and StringBuilder
-}
+ public class Diff{
+    // Show the Difference Between String and StringBuilder
+    public static void main(String[]args){
+        String name = "Bharath";
+        System.out.println("name is " +name);
+        name= name+"Manne";
+        System.out.println("full name is " +name);
+    
+    
+        StringBuilder na = new StringBuilder("Bharath");
+        System.out.println("before String builder "+na);
+        na.append("Manne");
+        System.out.println("after String builder "+na);
+    
+    }
+    }
