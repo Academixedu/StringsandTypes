dfe46ff6b536b267f939514fbef1143461c59d74
12Rameshwar <chavhanrameshwar13@gmail.com>
2024-08-28 22:44:57 +0530

    String

--- Strings.java
+++ Strings.java
@@ -1,3 +1,15 @@
 public class Strings{
-// Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+    // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+    public static void main(String[] args) {
+    
+    String s = "Rameshwar Chavhan";
+    System.out.println(reverse(s));
+    }
+    
+    public static String reverse(String s) {
+        for(int i =s.length()-1;i>=0;i--){
+            System.out.print(s.charAt(i));
+        }
+        return " ";
+    }
 }
