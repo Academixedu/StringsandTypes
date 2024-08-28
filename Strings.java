0d698d50425bf6e0181eaf0d2a6877f3317dd545
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-28 18:25:03 +0530

    from fayaz

--- Strings.java
+++ Strings.java
@@ -1,3 +1,14 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+    
+
+public static String reverseString(String name){
+    StringBuilder na = new StringBuilder(name);
+    return na.reverse().toString();
+}
+public static void main(String[] args) {
+    String value = "fayaz";
+    String name = reverseString(value);
+    System.out.println(name);
+}
 }
