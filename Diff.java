cfbc08cf4bd65ed54a39ceb0bc049f379269620b
chdkumar <chokkakulakumar1123@gmail.com>
2024-08-28 18:47:11 +0530

    Strings&Types

--- Diff.java
+++ Diff.java
@@ -1,3 +1,24 @@
 public class Diff{
-// Show the Difference Between String and StringBuilder
+// Show the  String and StringBuilder 
+
+    public static void main(String[] args) {
+
+        String s = "Hello";
+        String s1= "world";
+        String s2= s.concat(s1);
+        System.out.println(s2);
+        System.out.println(s.substring(0,5));
+
+
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append("world");
+        System.out.println(sb);
+        StringBuffer sb1 = new StringBuffer("Hello world ");
+        System.out.println(sb1.insert(6,","));
+        System.out.println(sb1.insert(0, sb));
+
+        
+
+
+    }   
 }
--- Strings.java
+++ Strings.java
@@ -1,3 +1,14 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+
+public static void main(String[] args) {
+    String s = "Hello";
+    System.out.println(reverse(s));
+}
+public static String reverse(String s) {
+    for (int i = s.length() - 1; i >= 0; i--) {
+        System.out.print(s.charAt(i));
+    }
+    return "";
+}
 }
