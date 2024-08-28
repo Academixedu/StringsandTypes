57787cd5fcfdc63597dc008da38b8ac66d4e1906
nayeem6836 <maseednayeem@gmail.com>
2024-08-28 20:15:59 +0530

    Update Diff.java

--- Diff.java
+++ Diff.java
@@ -1,15 +1,15 @@
 public class Diff{
 // Show the Difference Between String and StringBuilder
 public static void main(String[]args){
-    String name = "fayaz";
+    String name = "Nayeem";
     System.out.println("name is " +name);
-    name= name+"shaik";
+    name= name+"Maseed";
     System.out.println("full name is " +name);
 
 
-    StringBuilder na = new StringBuilder("fayaz");
+    StringBuilder na = new StringBuilder("Nayeem");
     System.out.println("before String builder "+na);
-    na.append("shaik");
+    na.append("Maseed");
     System.out.println("after String builder "+na);
 
 }
