ad6a7b5b0810e3120b7a81d27f21e67518c4f4c8
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-28 20:39:49 +0530

    strings

--- Diff.java
+++ Diff.java
@@ -1,3 +1,15 @@
 public class Diff{
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        String str2="Java";
+        String str3=str2.concat(str);
+        System.out.println(str3);
+
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append("Java");
+        System.out.println(sb);
+        
+    }
 // Show the Difference Between String and StringBuilder
 }
