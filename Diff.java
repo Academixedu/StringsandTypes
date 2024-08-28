8f13a05d079e3160b18b81ed2f98a247f0bcef9e
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-08-28 20:42:14 +0530

    kkkk

--- Diff.java
+++ Diff.java
@@ -1,3 +1,14 @@
 public class Diff{
 // Show the Difference Between String and StringBuilder
+public static void main(String[] args) {
+    
+         String s = "Hello";
+    s += " World!";
+    System.out.println("String: " + s); 
+
+
+    StringBuilder sb = new StringBuilder("Hello");
+    sb.append(" World!");
+    System.out.println("StringBuilder: " + sb); 
+}
 }
--- Strings.java
+++ Strings.java
@@ -1,3 +1,12 @@
 public class Strings{
 // Prepare  a Reverse Function With Return Type String and Call IT in Main Method
+public static String reverse(String input) {
+    return new StringBuilder(input).reverse().toString();
+}
+
+public static void main(String[] args) {
+    String original = "Hello World";
+    String reversed = reverse(original);
+    System.out.println(reversed); 
+}
 }
